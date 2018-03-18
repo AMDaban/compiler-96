febb1faf6415a94bd98f434d54a2981fa7e66819
Alireza <daban.am56@gmail.com>
2018-03-18 18:37:03 +0330

    Change make-lexer.sh & add cool.cl for test

--- PA2/make-lexer.sh
+++ PA2/make-lexer.sh
@@ -1,4 +1,12 @@
 CUR_PATH=`pwd`
-cd ../class/assignments/PA2
+
+CUR_DIR=${PWD##*/}
+while [ $CUR_DIR != "compiler-96" ]
+do
+    cd ..
+    CUR_DIR=${PWD##*/}
+done
+
+cd class/assignments/PA2
 sudo make lexer
 cd $CUR_PATH
