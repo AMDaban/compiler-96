779fa8a3f31b30f32f2843f40706df73a20f3478
Alireza <daban.am56@gmail.com>
2018-03-18 18:52:47 +0330

    Change make-lexer

--- PA2/scripts/make-lexer.sh
+++ PA2/scripts/make-lexer.sh
@@ -7,6 +7,10 @@ do
     CUR_DIR=${PWD##*/}
 done
 
+sudo cp PA2/cool.flex class/assignments/PA2
+sudo cp PA2/README class/assignments/PA2
+sudo cp PA2/test.cl class/assignments/PA2
+
 cd class/assignments/PA2
 sudo make lexer
 cd $CUR_PATH
