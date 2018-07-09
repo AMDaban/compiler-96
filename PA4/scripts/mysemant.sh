51fa11f4b25f3c20b125ad9b7e0ceab388f13320
Alireza <daban.am56@gmail.com>
2018-07-09 18:22:36 +0430

    finished PA$

---
+++ PA4/scripts/mysemant.sh
@@ -0,0 +1,14 @@
+CUR_PATH=`pwd`
+
+CUR_DIR=${PWD##*/}
+FILE_PATH=$CUR_PATH/$1
+while [ $CUR_DIR != "compiler-96" ]
+do
+    cd ..
+    CUR_DIR=${PWD##*/}
+done
+
+cd class/assignments/PA4/ 
+./mycoolc $FILE_PATH
+
+cd $CUR_PATH
